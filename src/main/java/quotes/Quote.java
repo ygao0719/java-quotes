411efd6805482eb21b08b1e307bc67230ddf5019
Yuan Gao <example@email.com>
2019-06-11 11:43:51 -0700

    done getQuote

--- src/main/java/quotes/Quote.java
+++ src/main/java/quotes/Quote.java
@@ -5,14 +5,17 @@ public class Quote {
     String[] tags;
     String author;
     String likes;
+    String text;
     String quoteText;
 
-    public Quote(String[] tags, String author, String likes, String text){
+    public Quote(String[] tags, String author, String likes, String text,String quoteText){
 
         this.tags = tags;
         this.author = author;
         this.likes = likes;
-        this.quoteText = text;
+        this.text = text;
+        this.quoteText = quoteText;
+
     }
     public String getAuthor(){
         return this.author;
@@ -23,10 +26,10 @@ public class Quote {
     }
 
     public String getText(){
-        return this.quoteText;
+        return this.text;
     }
     public String toString(){
-        return String.format("The quote %s \n is written by %s",this.quoteText,this.author);
+        return String.format("The quote %s \n is written by %s",this.text,this.author);
     }
 
 }
