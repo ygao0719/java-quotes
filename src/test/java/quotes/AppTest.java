b6c3e3de5254846432d9b2c25eea6c4446c3d0cc
Yuan Gao <example@email.com>
2019-06-12 18:34:12 -0700

    keep fix test

--- src/test/java/quotes/AppTest.java
+++ src/test/java/quotes/AppTest.java
@@ -36,7 +36,7 @@ public class AppTest {
     public void test_IOFile() {
         try {
             Gson gson = new Gson();
-
+            Quote quote = new Quote(,"harry","2 likes","If we expect to “know” the truth");
             FileWriter fileWriter = new FileWriter("./resources/test.json");
             gson.toJson(quoteList, fileWriter);
             fileWriter.close();
